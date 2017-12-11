a1c6b7f1e749e8233a1812c4299ff21d0b9b371a
Tống Tùng Giang <tongtunggiang@gmail.com>
2017-12-11 22:00:35 +0700

    Add random factor for the obstacles' individual boxes

---
+++ Assets/Scripts/ObstacleBoxModifier.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleBoxModifier : MonoBehaviour
+{
+    [SerializeField]
+    MinMaxConfig localVerticalPositionRange;
+
+    [SerializeField]
+    MinMaxConfig localVerticalScaleRange;
+
+    void OnSpawn()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, localVerticalPositionRange.RandomInsideRate);
+        transform.localScale = new Vector3(transform.localScale.x, localVerticalScaleRange.RandomInsideRate);
+    }
+}
